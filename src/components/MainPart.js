4396e8a29ed4bf903510c7ed1fd02befca96cec1
shaurya behra <shauryabehra23@gmail.com>
2025-09-01 22:00:17 +0530

    5th commit

--- src/components/MainPart.js
+++ src/components/MainPart.js
@@ -2,16 +2,19 @@ export default function Mainpart(props) {
     return (
         <main className="main-content">
             <div className={`content-section ${props.int % 2 === 0 ? 'even' : 'odd'}`}>
-                {props.int % 2 === 0 ? (
-                    <div className="content-flex">
+                <div className="section-container">
+                    <div className={`image-container ${props.int % 2 === 1 ? 'image-right' : 'image-left'}`}>
                         <img
                             src={props.pic}
                             alt=""
-                            className="content-image content-image-left"
+                            className="content-image"
                         />
-                        <div className="text-content">
-                            <div className="content-wrapper">
-                                <span className="section-number">{props.int.toString().padStart(2, '0')}</span>
+                    </div>
+                    
+                    <div className={`text-container ${props.int % 2 === 1 ? 'text-left' : 'text-right'}`}>
+                        <div className="content-wrapper">
+                            <span className="section-number">{props.int.toString().padStart(2, '0')}</span>
+                            <div className="text-content-inner">
                                 <h4 className="section-subtitle">{props.t1}</h4>
                                 <h2 className="section-title">{props.t2}</h2>
                                 <p className="section-text">{props.t3}</p>
@@ -19,24 +22,7 @@ export default function Mainpart(props) {
                             </div>
                         </div>
                     </div>
-                ) : (
-                    <div className="content-flex">
-                        <div className="text-content">
-                            <div className="content-wrapper">
-                                <span className="section-number">{props.int.toString().padStart(2, '0')}</span>
-                                <h4 className="section-subtitle">{props.t1}</h4>
-                                <h2 className="section-title">{props.t2}</h2>
-                                <p className="section-text">{props.t3}</p>
-                                <h4 className="read-more">read more →</h4>
-                            </div>
-                        </div>
-                        <img
-                            src={props.pic}
-                            alt=""
-                            className="content-image content-image-right"
-                        />
-                    </div>
-                )}
+                </div>
             </div>
         </main>
     )
